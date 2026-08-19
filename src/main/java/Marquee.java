8b2adf59de9de223e98b4514a5fe77e948d3cc75
Vu Tuan Long <225042007+vutuanlong07@users.noreply.github.com>
2026-08-20 02:37:34 +0800

    Add utility input function

--- src/main/java/Marquee.java
+++ src/main/java/Marquee.java
@@ -1,5 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Marquee {
-    public static void main(String[] args) {
+    private static final BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
         String banner = "____  ___\n" +
                 "|   \\/   |  _____   ____   _____   _   _   ____   ____\n" +
                 "| |\\  /| | /  _  /  | .-` /  _  \\ | | | | / ___) / ___)\n" +
@@ -11,6 +17,11 @@ public class Marquee {
         System.out.println(banner);
         greet();
         exit();
+    }
+
+    public static String getInput() throws IOException {
+        System.out.print("\n> ");
+        return inputReader.readLine();
     }
 
     public static void greet() {
