7470b54264e01988bc2f39b72dbe7265e0330f1d
Vu Tuan Long <225042007+vutuanlong07@users.noreply.github.com>
2026-08-26 03:44:38 +0800

    feat(dialogues): Added new dialogues
    New dialogue for corrupted cache file
    Minor changes to current dialogues

--- src/main/java/Dialogues.java
+++ src/main/java/Dialogues.java
@@ -1,12 +1,13 @@
 public final class Dialogues {
-    public static final String Banner = "____  ___\n" +
-            "|   \\/   |  _____   ____   _____   _   _   ____   ____\n" +
-            "| |\\  /| | / .__ /  | .-` / ._. \\ | | | | / ___) / ___)\n" +
-            "| | || | | | |_/ |  | |   | |_| | | |_| | | ___) | ___)\n" +
-            "|_| |/ |_| \\____/\\_ |_|   \\___  | \\_____\\ \\____) \\____)\n" +
-            "                              | |\n" +
-            "                              | |\n" +
-            "                               \\|";
+    public static final String Banner = """
+            ____  ___
+            |   \\/   |  _____   ____   _____   _   _   ____   ____
+            | |\\  /| | / .__ /  | .-` / ._. \\ | | | | / ___) / ___)
+            | | || | | | |_/ |  | |   | |_| | | |_| | | ___) | ___)
+            |_| |/ |_| \\____/\\_ |_|   \\___  | \\_____\\ \\____) \\____)
+                                          | |
+                                          | |
+                                           \\|""";
     public static final String Greetings = "Hi! I'm Marquee \\(>e<)/\nWhat will we do today? xD";
     public static final String Goodbye = "See you later :3";
 
@@ -16,22 +17,23 @@ public final class Dialogues {
     public static final String DeleteSuccessful = "These item(s) were deleted:\n%s\nThere are %d item(s) left in your checklist";
     public static final String AddTaskSuccessful = "Added task:\n  %s\nto the list (^_-☆ >c\nCurrently have %d item(s) in your checklist";
 
-    public static final String DisplayListEmpty = "No items yet -.- .·(ᶻzZ)";
-    public static final String MarkNoChange = "No items were marked (= ~ =)";
-    public static final String UnmarkNoChange = "No items were unmarked (= ~ =)";
-    public static final String DeleteNoChange = "No items were deleted (= ~ =)";
+    public static final String DisplayListEmpty = "No items yet (‾ 3‾)";
+    public static final String MarkNoChange = "No items were marked (‾ 3‾)";
+    public static final String UnmarkNoChange = "No items were unmarked (‾ 3‾)";
+    public static final String DeleteNoChange = "No items were deleted (‾ 3‾)";
 
-    public static final String MarkMissingArguments = "Indicate an index to mark -_-\"";
-    public static final String UnmarkMissingArguments = "Indicate an index to unmark -_-\"";
-    public static final String DeleteMissingArguments = "Indicate an index to delete -_-\"";
+    public static final String MarkMissingArguments = "Indicate an index to mark -_-'";
+    public static final String UnmarkMissingArguments = "Indicate an index to unmark -_-'";
+    public static final String DeleteMissingArguments = "Indicate an index to delete -_-'";
     public static final String TaskItemMissingName = "Task name can't be empty, duh °∀°?";
-    public static final String DeadlineMissing = "Missing deadline for task -_-\"";
-    public static final String EventMissingStart = "Missing start time for event -_-\"";
-    public static final String EventMissingEnd = "Missing end time for event -_-\"";
+    public static final String DeadlineMissing = "Missing deadline for task °∀°";
+    public static final String EventMissingStart = "Missing start time for event °∀°";
+    public static final String EventMissingEnd = "Missing end time for event °∀°";
 
-    public static final String IndexInvalid = "%d is not a valid index >_<";
+    public static final String CorruptedCacheFile = "..ca.che..fi.le..cor.rup.te..d.  Σ( ﾟДﾟ)";
+    public static final String IndexInvalid = "%d is not a valid index! >_<";
     public static final String IndexTooLarge = "%d is too large! Your checklist only has %d item(s) >_<";
-    public static final String IndexListEmpty = "Your checklist is empty! o Д o";
-    public static final String ArgumentNAN = "'%s' is not a number >_<";
-    public static final String UnknownCommand = "No clue what %s means `@ ᗝ @´╬";
+    public static final String IndexListEmpty = "Your checklist is empty...";
+    public static final String ArgumentNAN = "'%s' is not a number! >_<";
+    public static final String UnknownCommand = "No clue what %s means `O ᗝ O´╬";
 }
