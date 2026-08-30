a65c37f7e5c8ae24984471e2c8d1c794859befd2
Vu Tuan Long <225042007+vutuanlong07@users.noreply.github.com>
2026-08-30 23:38:19 +0800

    refactor(chatbot): Move dialogues container class into Marquee main class
    BREAKING CHANGE: Dialogues is now Marquee.Dialogues

--- src/main/java/Marquee.java
+++ src/main/java/Marquee.java
@@ -18,6 +18,47 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Marquee {
+    public static final class Dialogues {
+        public static final String Banner = """
+            ____  ___
+            |   \\/   |  _____   ____   _____   _   _   ____   ____
+            | |\\  /| | / .__ /  | .-` / ._. \\ | | | | / ___) / ___)
+            | | || | | | |_/ |  | |   | |_| | | |_| | | ___) | ___)
+            |_| |/ |_| \\____/\\_ |_|   \\___  | \\_____\\ \\____) \\____)
+                                          | |
+                                          | |
+                                           \\|""";
+        public static final String Greetings = "Hi! I'm Marquee \\(>e<)/\nWhat will we do today? xD";
+        public static final String Goodbye = "See you later :3";
+
+        public static final String DisplayListSuccessful = "Current item(s) in your list:";
+        public static final String MarkSuccessful = "These item(s) were marked:";
+        public static final String UnmarkSuccessful = "These item(s) were unmarked:";
+        public static final String DeleteSuccessful = "These item(s) were deleted:\n%s\nThere are %d item(s) left in your checklist";
+        public static final String AddTaskSuccessful = "Added task:\n  %s\nto the list (^_-☆ >c\nCurrently have %d item(s) in your checklist";
+
+        public static final String DisplayListEmpty = "No items yet (‾ 3‾)";
+        public static final String MarkNoChange = "No items were marked (‾ 3‾)";
+        public static final String UnmarkNoChange = "No items were unmarked (‾ 3‾)";
+        public static final String DeleteNoChange = "No items were deleted (‾ 3‾)";
+
+        public static final String MarkMissingArguments = "Indicate an index to mark -_-'";
+        public static final String UnmarkMissingArguments = "Indicate an index to unmark -_-'";
+        public static final String DeleteMissingArguments = "Indicate an index to delete -_-'";
+        public static final String TaskItemMissingName = "Task name can't be empty, duh °∀°?";
+        public static final String DeadlineMissing = "Missing deadline for task °∀°";
+        public static final String EventMissingStart = "Missing start time for event °∀°";
+        public static final String EventMissingEnd = "Missing end time for event °∀°";
+
+        public static final String CorruptedCacheFile = "..ca.che..fi.le..cor.rup.te..d.  Σ( ﾟДﾟ)";
+        public static final String IndexInvalid = "%d is not a valid index! >_<";
+        public static final String IndexTooLarge = "%d is too large! Your checklist only has %d item(s) >_<";
+        public static final String IndexListEmpty = "Your checklist is empty...";
+        public static final String ArgumentNAN = "'%s' is not a number! (@ ~ @)";
+        public static final String InvalidDateTime = "Not a valid date! (@ ~ @)";
+        public static final String UnknownCommand = "No clue what %s means `O ᗝ O´╬";
+    }
+
     private static final String CSV_SEPARATOR = ";";
 
     private static final Path checklistPath = Path.of("./checklist.csv");
@@ -143,14 +184,14 @@ public class Marquee {
         return inputReader.readLine();
     }
 
-    public static List<TaskItem> readCSV() throws IOException {
+    public static List<TaskItem> readCSV(Path file) throws IOException {
         try {
-            Files.createFile(checklistPath);
-            Files.writeString(checklistPath, String.join(CSV_SEPARATOR, "tag", "marked", "content", "start", "end"));
+            Files.createFile(file);
+            Files.writeString(file, String.join(CSV_SEPARATOR, "tag", "marked", "content", "start", "end"));
             return new ArrayList<>();
         } catch (FileAlreadyExistsException _) {
             List<TaskItem> checklist = new ArrayList<>();
-            String[] lines = Files.readString(checklistPath).split("\r\n");
+            String[] lines = Files.readString(file).split("\r\n");
             boolean corrupted = false;
 
             List<String> headers = Arrays.asList(lines[0].split(CSV_SEPARATOR));
@@ -199,8 +240,8 @@ public class Marquee {
         }
     }
 
-    public static void writeCSV(List<TaskItem> checklist) throws IOException {
-        Files.writeString(checklistPath, Stream.concat(
+    public static void writeCSV(List<TaskItem> checklist, Path file) throws IOException {
+        Files.writeString(file, Stream.concat(
             Stream.of(String.join(CSV_SEPARATOR, "tag", "marked", "content", "start", "end")),
             checklist.stream()
                     .map(taskItem -> String.join(CSV_SEPARATOR,
