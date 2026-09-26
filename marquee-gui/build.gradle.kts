b052c0a4548736f1744b8f4bfacfd4eff954b7dd
Vu Tuan Long <225042007+vutuanlong07@users.noreply.github.com>
2026-09-26 16:36:29 +0800

    ci: Remove unneccessary Gradle plugins
    Signed-off-by: Vu Tuan Long <225042007+vutuanlong07@users.noreply.github.com>

--- marquee-gui/build.gradle.kts
+++ marquee-gui/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     application
     id("java-common-conventions")
-    id("org.openjfx.javafxplugin") version "0.1.0"
     id("com.gradleup.shadow") version "9.5.1"
 }
 
@@ -10,16 +9,11 @@ version = findProperty(project.name + "-version")!!
 
 val jarName = findProperty("jarName") ?: "${project.name}-v${project.version}-all"
 
-javafx {
-    version = "26.0.2"
-    modules("javafx.controls", "javafx.graphics", "javafx.fxml")
-}
-
 dependencies {
     implementation(project(":marquee-core"))
 
     val jfxVersion = "26.0.2"
-    val jfxPlatforms = arrayOf("win", "linux", "mac")
+    val jfxPlatforms = arrayOf("win", "linux", "linux-aarch64", "mac", "mac-aarch64")
     for (platform in jfxPlatforms) {
         implementation("org.openjfx:javafx-base:$jfxVersion:$platform")
         implementation("org.openjfx:javafx-controls:$jfxVersion:$platform")
@@ -29,7 +23,7 @@ dependencies {
 }
 
 application {
-    mainClass = "org.cs2103t.marquee.gui.MainApplication"
+    mainClass = "org.cs2103t.marquee.gui.AlternativeMain"
 }
 
 tasks.jar {
